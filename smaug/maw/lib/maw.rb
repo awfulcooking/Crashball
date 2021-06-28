a638e028c19f3fb04527272797e58b51e278e867
mooff <mooff@together.beer>
2021-06-28 12:09:41 +0100

    Update to Maw v1.3.0

--- smaug/maw/lib/maw.rb
+++ smaug/maw/lib/maw.rb
@@ -2,18 +2,21 @@
 # https://github.com/togetherbeer/maw
 #
 # @copyright 2021 mooff <mooff@@together.beer>
-# @version 1.2.0
+# @version 1.3.0
 # @license AGPLv3
 
-$args = $gtk.args
 $outputs = $args.outputs
-$state = $args.state
 
 def Maw!
   extend Maw::Tick
 
   include Maw
   include Maw::Init
+  include Maw::Ergonomic
+  include Maw::Helpers
+end
+
+def ergonomic!
   include Maw::Ergonomic
 end
 
@@ -81,22 +84,37 @@ module Maw
       Kernel.tick_count
     end
 
-    def controls
+    def controls &blk
       $default_controls ||= ::Maw::Controls.new
+      $default_controls.instance_eval(&blk) if blk
+      $default_controls
     end
   end
 
   Ergonomics = Ergonomic # to be even more ergonomic
 
-  class Controls
-    @@i = 0
+  module Helpers
+    def background! color=[0,0,0]
+      $outputs.background_color = color
+    end
 
+    def sounds
+      $outputs.sounds
+    end
+  end
+
+  class Controls
+    attr_accessor :name
+    
+    @@i = 0    
     def initialize name="Control Set #{@@i+=1}", &blk
       @name = name
 
       instance_exec(&blk) if blk
       self
     end
+
+    def to_s; "[#{name}]"; end
 
     def is? state, device, key
       if device == :mouse # mouse doesn't support state qualifiers like .key_down, .key_held etc
