32ade4c30effb6826987f143e6b9a2ff087837fe
mooff <mooff@together.beer>
2021-06-28 11:36:25 +0100

    2nd Spike - Four players, brake

--- app/game.rb
+++ app/game.rb
@@ -4,91 +4,134 @@ controls.define :quit, keyboard: :q, controller_one: [:start], mouse: :button_mi
 controls.define :reset, keyboard: :r, controller_one: :r1
 controls.define :debug, keyboard: :g, controller_one: :b
 controls.define :debug_framerate, keyboard: :t, controller_one: :x
+controls.define :debug_players, keyboard: :p, controller_one: :l1
 
 controls.define :left, keyboard: :a, controller_one: :left
 controls.define :right, keyboard: :d, controller_one: :right
 controls.define :up, keyboard: :w, controller_one: :up
 controls.define :down, keyboard: :s, controller_one: :down
 
-controls.define :boost, keyboard: :shift, controller_one: [:l2, :r2]
+controls.define :brake, keyboard: :shift, controller_one: [:l2]
+controls.define :boost, keyboard: :space, controller_one: [:r2]
 controls.define :kick, keyboard: :e, controller_one: :a
 
-DDX_NORMAL = 0.92
+DDX_NORMAL = 0.85
 DDX_BOOST = 0.97
+DDX_BRAKE = 0.52
+
+PLAYER_HEIGHT = 20
+
+def player!(opts={})
+  {
+    x: 0, y: 0,
+    w: 0, h: 0,
+    r: rand(255), g: rand(255), b: rand(255),
+    rotation: 0.8,
+    vertical: false
+  }.merge! opts
+end
 
 init {
-    play_sound_effect
+  play_sound_effect
 
-    $state.background = [
-        0, 0, grid.w, grid.h,
-        rand(255), rand(255), rand(255)
-    ]
+  $state.background = [
+    0, 0, grid.w, grid.h,
+    rand(255), rand(255), rand(255)
+  ]
 
-    $state.player = {
-        x: 500, y: 50, w: 280, h: 10,
-        r: 255, g: 105, b: 199,
+  $state.players = {
+    top:    player!(y: grid.h-PLAYER_HEIGHT, w: grid.w, h: PLAYER_HEIGHT),
+    right:  player!(x: grid.w-PLAYER_HEIGHT, w: PLAYER_HEIGHT, h: grid.h, vertical: true),
+    bottom: player!(h: PLAYER_HEIGHT),
+    left:   player!(w: PLAYER_HEIGHT, h: grid.h, vertical: true),
+  }
 
-        dx: (3+rand(15)).rand_sign,
-        ddx: DDX_NORMAL
-    }
+  $state.players.each { |(position, player)|
+    max = player.vertical ? grid.h/2 : grid.w/2
+    position = max/2 + rand(200) - 100
+    size = 100 + rand(400)
 
-    $state.nets = [
-        {x: 0, y: 0, w: 20, h: grid.h},         # left
-        {x: grid.w-20, y: 0, w: 20, h: grid.h}, # right
-        {x: 0, y: 0, w: grid.w, h: 20},         # bottom
-        {x: 0, y: grid.h-20, w: grid.w, h: 20}, # top
-    ]
+    if player.vertical
+      player.y = position
+      player.h = size
+    else
+      player.x = position
+      player.w = size
+    end
+
+    player.dx = (5+rand(15)).rand_sign
+    player.ddx = DDX_NORMAL
+  }
+
+  $state.player = $state.players[:bottom]
+
+  $state.nets = {
+    top:    {x: 0, y: grid.h-20, w: grid.w, h: 20},
+    right:  {x: grid.w-20, y: 0, w: 20, h: grid.h},
+    bottom: {x: 0, y: 0, w: grid.w, h: 20},
+    left:   {x: 0, y: 0, w: 20, h: grid.h},
+  }
 }
 
 tick {
-    if controls.reset_down?
-        init
-    end
+  if controls.reset_down?
+    init
+  end
 
-    if controls.quit?
-        exit
-    end
+  if controls.quit?
+    exit
+  end
 
-    movement
-    bounds
-    
-    solids << $state.background
-    solids << $state.player
+  movement
+  bounds
+  
+  solids << $state.background
 
-    if controls.debug?
-        solids << $state.nets.map { |net|
-            {r: 0, g: 0, b: 200, a: 160}.merge! net
-        }
-    end
+  for player in $state.players.values
+    solids << player
+  end
 
-    if controls.debug_framerate?
-      primitives << $gtk.framerate_diagnostics_primitives
-    end
+  if controls.debug?
+    solids << $state.nets.values.map { |net|
+      {r: 0, g: 0, b: 200, a: 130}.merge! net
+    }
+  end
+
+  if controls.debug_framerate?
+    primitives << $gtk.framerate_diagnostics_primitives
+  end
 }
 
 def movement
-    if controls.left?
-        $state.player.dx -= 4
-    elsif controls.right?
-        $state.player.dx += 4
+  if controls.left?
+      $state.player.dx -= 4
+  elsif controls.right?
+    $state.player.dx += 4
+  end
+
+  $state.player.ddx = case true
+    when controls.brake?
+      DDX_BRAKE
+    when controls.boost?
+      DDX_BOOST
+    else
+      DDX_NORMAL
     end
 
-    $state.player.ddx = controls.boost? ? DDX_BOOST : DDX_NORMAL
-
-    $state.player.x += $state.player.dx
-    $state.player.dx *= $state.player.ddx
+  $state.player.x += $state.player.dx
+  $state.player.dx *= $state.player.ddx
 end
 
 def bounds
-    if $state.player.x < (0-$state.player.w)
-        $state.player.x = grid.w
-    elsif $state.player.x > grid.w
-        $state.player.x = 0-$state.player.w
-    end
+  if $state.player.x < (0-$state.player.w)
+    $state.player.x = grid.w
+  elsif $state.player.x > grid.w
+    $state.player.x = 0-$state.player.w
+  end
 end
 
 def play_sound_effect
-    audio[:sound_effect] = {
-        input: 'sounds/GameStart.wav'
-    }
+  audio[:sound_effect] = {
+    input: 'sounds/GameStart.wav'
+  }
 end
