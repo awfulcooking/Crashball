0038d9048fe30c41bf92725c31f679613bdab405
mooff <mooff@together.beer>
2021-06-30 19:54:55 +0100

    Update controls

--- app/game.rb
+++ app/game.rb
@@ -8,14 +8,14 @@ controls.define :debug, keyboard: :t, controller_one: :b
 controls.define :debug_framerate, keyboard: :t, controller_one: :x
 controls.define :debug_players, keyboard: :p, controller_one: :l1
 
-controls.define :left, keyboard: [:a, :left, :s, :z, :x], controller_one: :left
-controls.define :right, keyboard: [:d, :right, :f, :c, :v], controller_one: :right
+controls.define :left, keyboard: [:left, :s, :z, :x], controller_one: :left
+controls.define :right, keyboard: [:right, :f, :c, :v], controller_one: :right
 controls.define :up, keyboard: [:w, :up, :e], controller_one: :up
 controls.define :down, keyboard: [:w, :down, :c], controller_one: :down
 
-controls.define :brake, keyboard: [:g, :d], controller_one: [:l2]
-controls.define :boost, keyboard: [:shift], controller_one: [:r2]
-controls.define :kick, keyboard: [:e, :space], controller_one: :a
+controls.define :brake, keyboard: [:d], controller_one: [:l2]
+controls.define :boost, keyboard: [:a, :shift], controller_one: [:r2]
+controls.define :kick, keyboard: [:e, :g, :space], controller_one: :a
 
 ACCELERATION_NORMAL = 0.85
 ACCELERATION_MOVE = 0.9
